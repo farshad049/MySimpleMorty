b0f3e3a9e87828f86030f44973b9dee8c5a742b1
admin <farshad049@gmail.com>
2022-05-16 11:10:57 +0430

    header added to epoxy controller

--- app/src/main/java/com/example/mysimplemorty/characters/CharacterListPagingEpoxyController.kt
+++ app/src/main/java/com/example/mysimplemorty/characters/CharacterListPagingEpoxyController.kt
@@ -4,7 +4,10 @@ import com.airbnb.epoxy.EpoxyController
 import com.airbnb.epoxy.EpoxyModel
 import com.airbnb.epoxy.paging.PagedListEpoxyController
 import com.example.mysimplemorty.R
+import com.example.mysimplemorty.databinding.ModelCharacterDetailsHeaderBinding
 import com.example.mysimplemorty.databinding.ModelCharacterListModelBinding
+import com.example.mysimplemorty.databinding.ModelCharacterListTitleBinding
+import com.example.mysimplemorty.epoxy.LoadingEpoxyModel
 import com.example.mysimplemorty.epoxy.ViewBindingKotlinModel
 import com.example.mysimplemorty.network.responseModel.GetCharacterByIdResponse
 import com.squareup.picasso.Picasso
@@ -18,6 +21,28 @@ class CharacterListPagingEpoxyController:PagedListEpoxyController<GetCharacterBy
         return CharacterGridItem(item!!).id(item.id)
     }
 
+    //we have this in PagedListEpoxyController because don't implement required list in this file
+    override fun addModels(models: List<EpoxyModel<*>>) {
+        if (models.isEmpty()){
+            LoadingEpoxyModel().id("loading").addTo(this)
+            return
+        }
+        //add first five character with its header
+        Header("Main Family").id("main_family_header").addTo(this)
+        super.addModels(models.subList(0,5))
+
+       (models.subList(5,models.size) as List<CharacterGridItem>).groupBy {
+           it.item.name[0].uppercaseChar()
+       }.forEach {
+           //group by return a a map <key , List<item>>, key is this "it.item.name[0].uppercaseChar()" and the list is "models.subList(5,models.size) as List<CharacterGridItem>"
+           //we did this to could be able to use it ad id, because id should be unique because we are using it to grouping by
+            val key=it.key.toString().uppercase()
+            Header(key).id(key).addTo(this)
+           super.addModels(it.value)
+       }
+    }
+
+
 
 
     data class CharacterGridItem(val item:GetCharacterByIdResponse)
@@ -26,6 +51,18 @@ class CharacterListPagingEpoxyController:PagedListEpoxyController<GetCharacterBy
             tvCharacterName.text=item.name
             Picasso.get().load(item.image).into(ivCharacterImage)
         }
+    }
+
+    data class Header(val title:String)
+        :ViewBindingKotlinModel<ModelCharacterListTitleBinding>(R.layout.model_character_list_title){
+        override fun ModelCharacterListTitleBinding.bind() {
+            tvHeaderTitle.text=title
+        }
+        //let this to take whole span count
+        override fun getSpanSize(totalSpanCount: Int, position: Int, itemCount: Int): Int {
+            return totalSpanCount
+        }
+
     }
 
 
